c6f9d24f0e9b4978a5842d25947e42e46ab105a9
KYRSHANLANG R DKHAR <33800152+peace-shillong@users.noreply.github.com>
2019-08-12 00:41:39 +0530

    Day 6 : Using Gallery

---
+++ Day 6/Using Gallery/BirthdayApp/gen/com/example/birthdayapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.birthdayapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Day 6/Using Gallery/BirthdayApp/src/com/example/birthdayapp/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.birthdayapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.os.Build;
+import android.provider.MediaStore;
+
+public class MainActivity extends ActionBarActivity {
+
+	private static int RESULT_LOAD_IMAGE = 1;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+
+	public void loadImg(View v)
+	{
+		Intent i = new Intent(
+				Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);				
+				startActivityForResult(i, RESULT_LOAD_IMAGE);
+				
+	}
+	
+	 @Override
+	    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    	super.onActivityResult(requestCode, resultCode, data);
+	    	
+			if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+				Uri selectedImage = data.getData();
+				String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+				Cursor cursor = getContentResolver().query(selectedImage,
+						filePathColumn, null, null, null);
+				cursor.moveToFirst();
+
+				int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+				String picturePath = cursor.getString(columnIndex);
+				cursor.close();
+				
+				ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+				imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+			
+			}
+	    
+	    
+	    }
+}
